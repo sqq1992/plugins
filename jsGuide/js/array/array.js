2a7f3b29b4a6269898b37f86c3756f47b5000b0c
jojo <370029503@qq.com>
2016-11-09 02:42:10 +0800

    完成css3的拉杆动画！

--- jsGuide/js/array/array.js
+++ jsGuide/js/array/array.js
@@ -6,6 +6,32 @@ var a = ['a', , 'b', undefined, 'c'];
 //    console.log(a[i]);
 //}
 
-for(var i in a){
-    console.log(i);
+//for(var i in a){
+//    console.log(i);
+//}
+
+
+
+/**
+ * 从数组的位置搜索val值得指定位置，生成一个数组后返回
+ * @param arry
+ * @param val
+ */
+function findall(arry,val){
+    var newArry = [],
+        len = arry.length,
+        index = 0;
+
+    while(index<len){
+        index = arry.indexOf(val, index);
+        if(index===-1) break;
+        newArry.push(index);
+        index = index + 1;
+    }
+
+    return newArry;
+
 }
+
+var ar = findall([2, 3, 4, 5, 28, 2, 2], 2);
+console.log(ar);
